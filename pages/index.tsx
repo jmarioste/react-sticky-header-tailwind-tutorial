171d6e93ea5858c991d6bba11856f3e0ee208f5a
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2023-04-06 16:07:29 +0800

    Implement sticky header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
 import type { NextPage } from "next";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div className="container mx-auto">
-      <h1 className="text-4xl">NextJS + TailwindCSS Starter </h1>
+    <div className="">
+      <div className="h-40 grid place-content-center bg-indigo-900 text-gray-50">
+        <h1 className="text-2xl">Hello, world. Learn how to code</h1>
+      </div>
+      <Header />
+      <div className="container mx-auto">
+        <div className="p-4">
+          <h2 className="font-bold text-lg">Recent Posts</h2>
+          <p className="h-[2000px]">Some content</p>
+        </div>
+      </div>
     </div>
   );
 };
